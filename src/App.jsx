145bb1ff0a494f26fbf490abe038739d4637d8d3
q965786x <kasatka74@yandex.ru>
2025-10-29 19:13:19 +0700

    Из файла main.jsx убрала лишний импорт index.css, из файла App.jsx убрала лишний импорт useState и useEffect.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
