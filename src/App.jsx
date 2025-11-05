611909953614b7e30ecaca1158ae37b9bc3eab5b
q965786x <kasatka74@yandex.ru>
2025-11-05 21:21:59 +0700

    Разрешены конфликты

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import AppRoutes from './components/AppRoutes';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import PopExit from './components/PopExit/PopExit';
@@ -10,6 +11,7 @@ import PopBrowse from './components/PopBrowse/PopBrowse';
 const App = () => {  
 
   return (
+
     <div className="wrapper">
       {/* Pop-up компоненты */}
       <PopExit />
